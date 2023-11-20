d515b0fc36924609899a4223d1776e841c310bfb
EriksK82 <eriks.karklins82@gmail.com>
2023-11-20 13:10:02 +0200

    f_1

--- WPF Balloon popping game/WPF Balloon popping game/MainWindow.xaml.cs
+++ WPF Balloon popping game/WPF Balloon popping game/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Threading;
 
 namespace WPF_Balloon_popping_game
 {
+
         public partial class MainWindow : Window
     {
         DispatcherTimer gameTimer = new DispatcherTimer();
